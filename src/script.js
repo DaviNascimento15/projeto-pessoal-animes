82fa5ae8ce549aceb41542693ecf747099cae87c
Davi Nascimento <francisco.nascimento1908@aluno.ce.gov.br>
2025-03-14 14:17:31 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Site Anime Universe carregado com sucesso!");
+});
